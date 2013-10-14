c7431fa12d52c36dd9ec18861a4d4311f23def1d
yalong.li <573260700@qq.com>
2013-10-15 01:19:02 +0800

    caviar dataset helper

---
+++ dataHelpers_new2d/caviar.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 23:38:26 2013
+
+@author: yalong.li
+"""
+
+# caviar
+import my_ulity
+import os
+import cv2
+
+def caviar_video2image():
+    rgb_des_root_dir = 'H:\\new_datasets_2d\\caviar'
+    root_dir = 'C:\\wget\\CAVIAR\\CAVIAR'
+    
+    action_types = os.listdir(root_dir)
+    
+    for action_type in action_types:
+        action_folder = os.path.join(root_dir, action_type)        
+            
+        samples_names = os.listdir(action_folder)
+            
+        for sample_name in samples_names:
+                
+            # get video path, and read video
+            video_path = os.path.join(action_folder, sample_name)
+            video_cap = cv2.VideoCapture(video_path)
+            
+            if not video_cap:
+                continue
+            else:
+                
+                # create destination folder
+                rgb_des_curr = os.path.join(rgb_des_root_dir, action_type)
+                my_ulity.create_des_folder(rgb_des_curr)
+            
+                my_ulity.video2image(video_cap, rgb_des_curr)
+        
+        print 'finished', action_type, 'folder ...'
+                
+
+def main():
+    caviar_video2image()
+    print 'OK...'
+    
+if __name__ == '__main__':
+    main()
